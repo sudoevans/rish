cf8e3d2592dc3b29a563171fd734d9ca95823f60
Kiptoo <75899973+gamer-snave@users.noreply.github.com>
2022-05-24 21:31:08 +0300

    Update pentagonal.py

--- pentagonal.py
+++ pentagonal.py
@@ -1,20 +1,5 @@
-# import math
-# def isPentagonalNumber(n):
-#     if (1 + math.sqrt(1 + 24 * n)) % 6 == 0:
-#         return True
-#     else:
-#         return False
-
-# def main():
-#     for i in range(1, 101):
-#         if isPentagonalNumber(i):
-#             print(i, end = " ")
-#         if i % 10 == 0:
-#             print()
-# main()
-
-
-# The expected code:
+#! python3
+# Code to display first 100 pentagonal numbers Withoud using Math Module
 
 n = 1
 while n <= 100:
